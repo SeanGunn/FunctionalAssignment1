be1e98d22643d959a558f2463df188517cfe3c43
SeanGunn <44640955+SeanGunn@users.noreply.github.com>
2020-01-11 11:11:14 +0000

    finished

--- assignment1fp/src/assignment1fp/core.clj
+++ assignment1fp/src/assignment1fp/core.clj
@@ -106,60 +106,76 @@
 
 (Q2 100 :quarter {:dollar 0 :halfDollars 0 :quarter 0 :dime 0 :nickel 0 :penny 0} 6)
 (Q2 100 :quarter {:dollar 0 :halfDollars 0 :quarter 0 :dime 0 :nickel 0 :penny 0} 6)
+;;;;;;;;q3
+(def Q3
+  ;;using random int i am adding a plant randomly to windowPlants
+(( let windowPlants [] ((for [i (range 48)] (let rn(rand-int 4)
+                                                 ;;if random number equals 0 adds grass
+                     (if (= rn 0)
+                       (do conj [g])
+                       )
+                                                 ;;if random number equals 1 adds clover
+                     (if (= rn 1)
+                       (do conj [c]))
+                                                 ;;if random number equals 2 adds radishes
+                     (if (= rn 2)
+                       (do conj [r]))
+                                                 ;;if random number equals 3 adds violets
+                     (if (= rn 3)
+                       (do conj [v])))))
+                    ;;calling many kids with different spellings to find their plants
+                    (whosPlantOnePersonAsk windowPlants "ALICE")
+                    (whosPlantOnePersonAsk windowPlants "eVE")
+                    (whosPlantOnePersonAsk windowPlants "Ileana")
+                    (whosPlantOnePersonAsk windowPlants "larry")
+                    (whosPlantOnePersonAsk windowPlants ""))
+ )
+
+(defn whosPlantOnePersonAsk [plants person]
+  ;;if the kid that is passed through is the same spelling as the preset kid then they will get their plants after the person value is edit to be a lowercase
+  (if (= clojure.string/lower-case person "alice")
+    (do (print "Alice's plants are" (nth plants 0) (nth plants 1) (nth plants 2) (nth plants 3))))
+  (if (= clojure.string/lower-case person "bob")
+    (do (print "Bob's plants are" (nth plants 4) (nth plants 5) (nth plants 6) (nth plants 7) )))
+  (if (= clojure.string/lower-case person "charlie")
+    (do (print "Charlie's plants are" (nth plants 8) (nth plants 9) (nth plants 10) (nth plants 11))))
+  (if (= clojure.string/lower-case person "david")
+    (do (print "David's plants are" (nth plants 12) (nth plants 13) (nth plants 14) (nth plants 15))))
+  (if (= clojure.string/lower-case person "eve")
+    (do (print "Eve's plants are" (nth plants 16) (nth plants 17) (nth plants 18) (nth plants 19))))
+  (if (= clojure.string/lower-case person "fred")
+    (do (print "Fred's plants are" (nth plants 20) (nth plants 21) (nth plants 22) (nth plants 23))))
+  (if (= clojure.string/lower-case person "ginny")
+    (do (print "Ginny's plants are" (nth plants 24) (nth plants 25) (nth plants 26) (nth plants 27))))
+  (if (= clojure.string/lower-case person "harriet")
+    (do (print "Harriet's plants are" (nth plants 28) (nth plants 29) (nth plants 30) (nth plants 31))))
+  (if (= clojure.string/lower-case person "ileana")
+    (do (print "Ileana's plants are" (nth plants 32) (nth plants 33) (nth plants 34) (nth plants 35))))
+  (if (= clojure.string/lower-case person "joseph")
+    (do (print "Joseph's plants are" (nth plants 36) (nth plants 37) (nth plants 38) (nth plants 39))))
+  (if (= clojure.string/lower-case person "kincaid")
+    (do (print "Kincaid's plants are" (nth plants 40) (nth plants 42) (nth plants 43) (nth plants 44))))
+  (if (= clojure.string/lower-case person "larry")
+    (do (print "Larry's plants are" (nth plants 45) (nth plants 46) (nth plants 47) (nth plants 48))))
+  )
+
 
 ;;;;;;;;q4
 (defn Q4
   ((let nasa (slurp "https://data.nasa.gov/resource/y77d-th95.json"))
-   (yearMostMeterFalls nasa))
+   (yearMostMeterFalls nasa)
+   (yearHeaviestMeterFall  nasa)
+   (yearMostCommon nasa)
+   (greatestReclat nasa)
+   (greatestReclong nasa)))
 
-  )
-
-(defn yearMostMeterFalls [data]
-  ;;defining the values to compare
-  ((let yearMostCurrent (= 0))
-   (let yearMostPossiable)
-  (let amountCurrentMost (= 0))
-   (let amountCurrentPoss (= 0))
-   (let numberInList)
-   ;;creating the tail recursion
-      (defn yearFallsTail [n ] (
-                                ;;if it has a fell as a value in the n th part of the data it will enter the if
-      (if (= (nth n data (:fall)) "Fell")
-        ;;if never set a value for year it will set one as base
-        ((if (zero? yearMostCurrent)
-           ;;setting year most current as nth data year
-           ( yearMostCurrent (= (nth n data (:year))))
-           ;;increasing amount
-                                    (inc amountCurrentMost)
-           ;;setting the refinder as n
-                                    (nInList (= n)))
-         )
-        ;;if yearMost possible is greater than yearMostCurrent its gonna set the values to it
-        (if (< yearMostCurrent yearMostPossiable)
-           (= yearMostCurrent yearMostPossiable)
-           (inc amountCurrentPossiable)
-           (numberInList (= n)))
-        ;;if it falls in year same as yearMostCurrent increase its counter
-        (if (= (nth n data (:year)) yearMostCurrent)
-          (inc yearMostCurrent)
-          )
-        ;;if it falls in year same as yearMostPossiable increase its counter
-        (if (= (nth n data (:year)) yearMostPossiable)
-          (inc yearMostPossiable)
-          ))
-                                ;;if n is the same size as the data size the call yearMostMeterFallsFinal
-                                (if (= n (count data))
-                    (yearMostMeterFallsFinal numberInList data)))
-        (recur (inc n)))))
 
 (defn yearMostMeterFalls [data]
-  ;;using regex to filter through the code and get a value that has the frequencies
+  ;;using regex to filter through the code and get a value that, then using frequencies to par the values together so i can count then sort the values to display after
 ((let amount (sort (count (frequencies (filter #("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))")) ))))
  ;;calling the function to print out the top sorted answer
  (yearMostMeterFallsAnswer (first amount))
   )
-
-
 
   (defn yearMostMeterFallsAnswer [year]
   ;;just prints out the year with most meterfalls
@@ -167,47 +183,47 @@
   )
 
 (defn yearHeaviestMeterFall [data]
-  ;;defining the values to compare
-  ((let yearMostCurrent (= 0))
-   (let yearMostPossiable)
-   (let amountCurrentMost (= 0))
-   (let amountCurrentPoss (= 0))
-   (let numberInList)
-   (let howHeavy1 (= 0))
-   (let howHeavy2 (= 0))
-   ;;creating the tail recursion
-   (defn yearHeaviestTail [n ] (
-                                ;;if it has a fell as a value in the n th part of the data it will enter the if
-                             (if (= (nth n data (:fall)) "Fell")
-                               ;;if never set a value for year it will set one as base
-                               ((if (zero? yearMostCurrent)
-                                  ( yearMostCurrent (= (nth n data (:year))))
-                                  (inc amountCurrentMost)
-                                  (nInList (= 0))
-                                  (howHeavy1 (= (+ (howHeavy1 (nth n data (:mass)))))))
-                                )
-                               ;;if yearMostPossible is greater than yearMostCurrent its gonna set the values to it and where to find it later
-                               (if (< yearMostCurrent yearMostPossiable)
-                                 (= yearMostCurrent yearMostPossiable)
-                                 (inc amountCurrentPossiable)
-                                 (numberInList (= n))
-                                 (howHeavy1 (= howHeavy2)))))
-  ;;if it falls in year same as yearMostCurrent increase its counter and increases its mass
-                               (if (= (nth n data (:year)) yearMostCurrent)
-                                 (inc yearMostCurrent)
-                                 (howHeavy1 (= (+ (howHeavy1 (nth n data (:mass)))))))
-                                 )
-;;if it falls in year same as yearMostPossiable increase its counter and increases its mass
-                               (if (= (nth n data (:year)) yearMostPossiable)
-                                 (inc yearMostPossiable)
-                                 (howHeavy2 (= (+ (howHeavy2 (nth n data (:mass)))))))
-;;if n is same size as the data size then calls yearHeaviestMeterFallFinal to display the answer
-                                (if (= n (count data))
-                                  (yearHeaviestMeterFallFinal numberInList data howHeavy1))
-     (recur (inc n))))
+  ;;using regex to filter through the code and get a value that, then using frequencies to par the values together then sorting by date to get them in order then count the values together to add them
+  ((let meterFallSorted (count (sort-by :date (frequencies (filter #("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))")))))))
+   ;;calling the function to print out the top sorted answer
+   (yearHeaviestMeterFallFinal (first meterFallSorted))))
 
-(defn yearHeaviestMeterFallFinal [numberInList data mass]
+
+
+(defn yearHeaviestMeterFallFinal [numberInList]
   ;;prints out the year with the heaviest collective meteor fall
-  (print "year with the heaviest MeterFalls was " (nth numberInlist data (:year)))
-  (print "with a mass of "mass)
+  (print "year with the heaviest MeterFalls was " numberInList))
+
+(defn yearMostCommon [data]
+  ;;using regex to filter through the code and get a value that, then using frequencies to par the values together then sorting them by the most common date to display afterwards
+  ((let yearMostCommonSorted (sort-by :date (frequencies (filter #("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))"))))))
+  ;;calling the function to print out the top sorted answer
+  (yearMostCommonFinal (first yearMostCommonSorted))
   )
+
+
+(defn yearMostCommonFinal [numberInList]
+  ;;prints out the year with the heaviest collective meteor fall
+  (print "the year that was the most common was " numberInList))
+
+(defn greatestReclat [data]
+  ;;using regex to filter through the code and get a value that, then using frequencies to par the values together then sorting them by the greatest reclat value to display afterwards
+  ((let greatestReclatSorted (sort-by > val (frequencies (filter #("[\"]\\w{6}[\"][:][\"][-]?\\d+.\\d\\d\\d\\d\\d\\d"))))))
+  ;;calling the function to print out the top sorted answer
+  (yearGreatestReclatFinal (first greatestReclatSorted))
+  )
+
+(defn yearGreatestReclatFinal [numberInList]
+  ;;prints out the greatest reclate
+  (print "the greatest reclat was " numberInList))
+
+(defn greatestReclong [data]
+  ;;using regex to filter through the code and get a value that, then using frequencies to par the values together then sorting them by the greatest reclong value to display afterwards
+  ((let greatestReclongSorted (sort-by > val (frequencies (filter #("[\"]\\w{7}[\"][:][\"][-]?\\d+.\\d\\d\\d\\d\\d\\d"))))))
+  ;;calling the function to print out the top sorted answer
+  (yearGreatestReclongFinal (first greatestReclongSorted))
+  )
+
+(defn yearGreatestReclongFinal [numberInList]
+  ;;prints out the greatest reclong
+  (print "the greatest reclong was " numberInList))
